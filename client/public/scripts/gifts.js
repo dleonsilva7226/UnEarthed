9cdc8ba453822c4d6651231be5ce34d1297ded19
dleonsilva7226 <dleonsilva7226@gmail.com>
2024-11-25 21:10:57 -0500

    Implement UnEarthed
    Implemented UnEarthed. However, there is still a small bug in which clicking on the 'read more' link does not work unfortunately

--- client/public/scripts/gifts.js
+++ client/public/scripts/gifts.js
@@ -51,4 +51,41 @@ const renderGifts = async () => {
     }
 }
 
-renderGifts()
+const renderGift = async () => {
+    
+    const requestedID = parseInt(window.location.href.split('/').pop())
+    const response = await fetch('/gifts')
+    const data = await response.json()
+
+    const giftContent = document.getElementById('gift-content')
+    let gift = null
+
+    if (data) {
+        gift = data.find((currGift) => (currGift.id === requestedID))
+    }
+    if (gift) {
+        document.getElementById('image').src = gift.image
+        document.getElementById('name').textContent = gift.name
+        document.getElementById('submittedBy').textContent = 'Submitted by: ' + gift.submittedBy
+        document.getElementById('pricePoint').textContent = 'Price: ' + gift.pricePoint
+        document.getElementById('audience').textContent = 'Great For: ' + gift.audience
+        document.getElementById('description').textContent = gift.description
+        document.title = `UnEarthed - ${gift.name}`
+    } else {
+        const message = document.createElement('h2')
+        message.textContent = `No Gifts Available 😞`
+        giftContent.appendChild(message)
+    }
+}
+
+// 404 page error
+const requestedURL = window.location.href.split('/').pop()
+if (requestedURL) {
+   window.location.href = `../404.html` 
+}
+else {
+    renderGifts()
+}
+
+
+renderGift()
